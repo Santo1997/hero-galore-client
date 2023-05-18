49a5553fe05afaa07e6de952bb231ac7fbb5b03e
Santo1997 <hossainsanto2@gmail.com>
2023-05-18 15:46:33 +0600

    login authenticate

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,7 +1,32 @@
 import React, { useContext, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../provider/AuthProvider";
 
 const Login = () => {
+  const { user, signIn } = useContext(AuthContext);
+  const [err, setErr] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    setErr("");
+    const form = event.target;
+    const email = form.email.value;
+    const pass = form.pass.value;
+
+    signIn(email, pass)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        form.reset();
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        setErr(errorMessage);
+      });
+  };
   return (
     <div className="hero min-h-[calc(100vh-300px)] text-black">
       <div className="hero-content flex-col w-full lg:w-3/6">
@@ -9,7 +34,7 @@ const Login = () => {
           <h1 className="text-5xl font-bold mb-10 ">Login now!</h1>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
-          <form className="card-body">
+          <form onSubmit={handleLogin} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text text-black text-lg font-bold">
@@ -38,6 +63,11 @@ const Login = () => {
                 required
               />
             </div>
+            {err && (
+              <>
+                <p className="text-sm text-red-600 mt-5">{err}</p>
+              </>
+            )}
             <div className="form-control m-5">
               <button type="submit" className="btn btn-primary">
                 Login
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,7 +1,10 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
   signOut,
 } from "firebase/auth";
 import app from "../firebase/firebase.init";
@@ -19,14 +22,30 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
   const logOut = () => {
     return signOut(auth);
   };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   const authInfo = {
     user,
     loading,
     createUser,
+    signIn,
     logOut,
   };
 
