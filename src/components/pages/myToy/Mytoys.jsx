fa359b492635be1e28299c52b4788183389ceaf8
Santo1997 <hossainsanto2@gmail.com>
2023-05-19 16:04:15 +0600

    fetch mytoys

--- src/components/pages/myToy/Mytoys.jsx
+++ src/components/pages/myToy/Mytoys.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
 
 const Mytoys = () => {
+  const { user } = useContext(AuthContext);
+  const [myToy, setMyToy] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/usertoy?user=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setMyToy(data));
+  }, []);
+
   let log =
     "https://i.ibb.co/f27tYN5/depositphotos-411005388-stock-photo-profile-picture-of-smiling-30s.webp";
   return (
     <div className="overflow-x-auto w-full pb-5">
       <table className="table text-white  w-full ">
-        {/* head */}
         <thead className="text-center">
           <tr>
             <th className="bg-black">Toy Image</th>
@@ -20,118 +29,37 @@ const Mytoys = () => {
           </tr>
         </thead>
         <tbody>
-          {/* row 1 */}
-          <tr>
-            <td className="bg-gray-500">
-              <div className="flex items-center space-x-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src={log} />
+          {myToy.map((toy) => (
+            <tr>
+              <td className="bg-gray-500">
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img src={log} />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </td>
-            <td className="bg-gray-500">
-              <div className="font-bold">Hart Hagerty</div>
-            </td>
-            <td className="bg-gray-500">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea,
-              mollitia.
-            </td>
-            <td className="bg-gray-500">Categoty</td>
-            <td className="bg-gray-500">Price</td>
-            <td className="bg-gray-500">Rating</td>
-            <td className="bg-gray-500">Quantity</td>
-            <td className="grid gap-3 bg-gray-500">
-              <button className="btn btn-xs btn-outline  btn-success">
-                Update
-              </button>
-              <button className="btn btn-xs btn-outline  btn-error ">
-                Delete
-              </button>
-            </td>
-          </tr>
-          {/* row 2 */}
-          <tr>
-            <td className="bg-gray-500">
-              <div className="flex items-center space-x-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src={log} />
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td className="bg-gray-500">
-              <div className="font-bold">Hart Hagerty</div>
-            </td>
-            <td className="bg-gray-500">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea,
-              mollitia.
-            </td>
-            <td className="bg-gray-500">Categoty</td>
-            <td className="bg-gray-500">Price</td>
-            <td className="bg-gray-500">Rating</td>
-            <td className="bg-gray-500">Quantity</td>
-            <td className="bg-gray-500">
-              <button>Update</button>
-              <button>Delete</button>
-            </td>
-          </tr>
-          {/* row 3 */}
-          <tr>
-            <td className="bg-gray-500">
-              <div className="flex items-center space-x-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src={log} />
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td className="bg-gray-500">
-              <div className="font-bold">Hart Hagerty</div>
-            </td>
-            <td className="bg-gray-500">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea,
-              mollitia.
-            </td>
-            <td className="bg-gray-500">Categoty</td>
-            <td className="bg-gray-500">Price</td>
-            <td className="bg-gray-500">Rating</td>
-            <td className="bg-gray-500">Quantity</td>
-            <td className="bg-gray-500">
-              <button>Update</button>
-              <button>Delete</button>
-            </td>
-          </tr>
-          {/* row 4 */}
-          <tr>
-            <td className="bg-gray-500">
-              <div className="flex items-center space-x-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src={log} />
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td className="bg-gray-500">
-              <div className="font-bold">Hart Hagerty</div>
-            </td>
-            <td className="bg-gray-500">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea,
-              mollitia.
-            </td>
-            <td className="bg-gray-500">Categoty</td>
-            <td className="bg-gray-500">Price</td>
-            <td className="bg-gray-500">Rating</td>
-            <td className="bg-gray-500">Quantity</td>
-            <td className="bg-gray-500">
-              <button>Update</button>
-              <button>Delete</button>
-            </td>
-          </tr>
+              </td>
+              <td className="bg-gray-500">
+                <div className="font-bold">{toy.toyName}</div>
+              </td>
+              <td className="bg-gray-500">
+                {toy.description.slice(0, 35) + "...."}
+              </td>
+              <td className="bg-gray-500">{toy.category}</td>
+              <td className="bg-gray-500">{toy.price}</td>
+              <td className="bg-gray-500">{toy.rating}</td>
+              <td className="bg-gray-500">{toy.quantity}</td>
+              <td className="grid gap-3 bg-gray-500">
+                <button className="btn btn-xs btn-outline  btn-success">
+                  Update
+                </button>
+                <button className="btn btn-xs btn-outline  btn-error ">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
